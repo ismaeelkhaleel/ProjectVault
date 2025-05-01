97347c3bb96dcdaa52bb17e809b813433c17f046
Mohd Ismaeel <pmohd367@gmail.com>
2025-05-02 02:00:20 +0530

    some changes

--- backend/controllers/userActivity.controller.js
+++ backend/controllers/userActivity.controller.js
@@ -1,36 +1,34 @@
 import UserActivity from "../models/userActivity.model.js";
 import mongoose from "mongoose";
 export const heatMap = async (req, res) => {
-    try {
-      const userId = req.params.id;
-  
-      const activityData = await UserActivity.aggregate([
-        {
-          $match: {
-            userId: new mongoose.Types.ObjectId(userId),
-          },
+  try {
+    const userId = req.params.id;
+
+    const activityData = await UserActivity.aggregate([
+      {
+        $match: {
+          userId: new mongoose.Types.ObjectId(userId),
         },
-        {
-          $group: {
-            _id: {
-              $dateToString: { format: "%Y-%m-%d", date: "$activityDate" },
-            },
-            count: { $sum: 1 },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: { format: "%Y-%m-%d", date: "$activityDate" },
           },
+          count: { $sum: 1 },
         },
-        { $sort: { _id: 1 } },
-      ]);
-  
-      const formattedData = activityData.map((item) => ({
-        date: item._id,
-        count: item.count,
-      }));
-  
-      console.log(formattedData);
-      res.json(formattedData);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Server Error");
-    }
-  };
-  
+      },
+      { $sort: { _id: 1 } },
+    ]);
+
+    const formattedData = activityData.map((item) => ({
+      date: item._id,
+      count: item.count,
+    }));
+
+    res.json(formattedData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+};
